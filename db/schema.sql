b439f76deab5b5fd241ccd76338b1aa1cfde0c17
fredm23579 <motta@g.ucla.edu>
2024-05-07 05:45:34 +0100

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- Create the database
+CREATE DATABASE ecommerce_db;
+
+-- Use the ecommerce_db database
+USE ecommerce_db;
